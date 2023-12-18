53c46789a2fc23d7f128f3d26667a9aa86463568
Jeff T14s <jehonor@outlook.com>
2023-12-19 00:20:33 +0100

    lets see

--- main.tf
+++ main.tf
@@ -5,25 +5,13 @@ terraform {
       version = "~>3.0"
     }
   }
-  backend "azurerm" {
-      resource_group_name  = "iac"
-      storage_account_name = "terraform202316"
-      container_name       = "tfstate"
-      key                  = "terraform.tfstate"
-      use_oidc             = true
-      client-id:          = var.client_id
-      tenant-id:           = var.tenant_id
-      subscription-id      = var.subscription_id
-  }
-
 }
-
 
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "state-demo-secure" {
-  name     = "state2-demo"
+  name     = "statfe-demo"
   location = "eastus"
 }
