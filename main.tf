c83ff4f3c1ed9c8046094be80efeb8ff70452177
Jeff T14s <jehonor@outlook.com>
2023-12-19 00:14:28 +0100

    lets see

--- main.tf
+++ main.tf
@@ -1,8 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name  = "iac"
+      storage_account_name = "terraform202316"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+      use_oidc             = true
+      client-id:          = var.client_id
+      tenant-id:           = var.tenant_id
+      subscription-id      = var.subscription_id
+  }
+
+}
+
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "subscription_id" {}
+
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "state-demo-secure" {
-  name     = "test-demo"
+  name     = "state2-demo"
   location = "eastus"
 }
