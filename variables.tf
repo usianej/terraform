0589ce40fdd4a021b63a6d64b65700d72df94143
Jeff T14s <jehonor@outlook.com>
2023-12-21 08:46:00 +0100

    testing

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "subscription_id" {}
-variable "client_id"       {}
-variable "client_secret"   {}
-variable "tenant_id"       {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
