e6058205dc5087f84b9cfdea5d46dec89601e713
Jeff T14s <jehonor@outlook.com>
2023-12-21 06:22:05 +0100

    used terraform format

--- main.tf
+++ main.tf
@@ -5,11 +5,11 @@ terraform {
       version = "~>3.2.0"
     }
   }
-backend "azurerm" {
-      resource_group_name  = "tfstate"
-      storage_account_name = "tfstate19531"
-      container_name       = "tfstate"
-      key                  = "terraform.tfstate"
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate19531"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 
 }
