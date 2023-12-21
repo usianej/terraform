24442f492fef59c4f150ab1be5933356bcb41193
Jeff T14s <jehonor@outlook.com>
2023-12-21 09:41:55 +0100

    Revert "Reverting back to CLI login"
    This reverts commit b34d52c7def1a9977eb5e47e3bf3b2be9a3faea4.

--- main.tf
+++ main.tf
@@ -19,10 +19,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
 }
 
 resource "azurerm_resource_group" "state-demo-secure" {
