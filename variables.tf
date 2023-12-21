3b407a31b085810d24fd816cf917daf0e81fb5a5
Jeff T14s <jehonor@outlook.com>
2023-12-21 09:20:58 +0100

    testing

--- variables.tf
+++ variables.tf
@@ -1,4 +1,13 @@
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
+variable "subscription_id" {
+  default = var.TF_VAR_subscription_id
+}
+variable "client_id" {
+  default = var.TF_VAR_client_id
+}
+variable "client_secret" {
+  default = var.TF_VAR_client_secret
+}
+variable "tenant_id" {
+  default = var.TF_VAR_tenant_id
+}
+
