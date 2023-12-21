866067aa49c36d4551dd854b0d05d6697aa143a4
Jeff T14s <jehonor@outlook.com>
2023-12-21 23:16:21 +0100

    less is sometimes better

--- main.tf
+++ main.tf
@@ -23,10 +23,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  use_oidc = true
 }
 
 resource "azurerm_resource_group" "state-demo-secure" {
