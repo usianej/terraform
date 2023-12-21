d2ee3907e82b91fa8c2bc38317c5bad404277f87
Jeff T14s <jehonor@outlook.com>
2023-12-21 07:48:11 +0100

    added oidc configuration to main.tf

--- main.tf
+++ main.tf
@@ -10,17 +10,15 @@ terraform {
     storage_account_name = "tfstate19531"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
+    use_oidc             = true
+    subscription_id      = "e77db16d-5ea4-4932-b1f7-066315e7b399"
+    tenant_id            = "9292348b-7b8b-44e5-a9c9-5c933c9e33da"
   }
 
 }
 
 provider "azurerm" {
   features {}
-
-  client_id       = "e6dce29d-7530-4781-b686-de40b09d5502"
-  client_secret   = "bDL8Q~b7U~InKQ~la5SRB_zcFEbKCruiTUnqocBd"
-  tenant_id       = "9292348b-7b8b-44e5-a9c9-5c933c9e33da"
-  subscription_id = "e77db16d-5ea4-4932-b1f7-066315e7b399"
 }
 
 resource "azurerm_resource_group" "state-demo-secure" {
