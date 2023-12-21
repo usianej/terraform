b9c5ce81f966e66d84446005c515e86b70877ef2
Jeff T14s <jehonor@outlook.com>
2023-12-21 16:38:24 +0100

    SO HELP ME GOD

--- main.tf
+++ main.tf
@@ -24,8 +24,8 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
-  client_id       = var.client_id != "" ? var.client_id : var.TF_VAR_client_id
-  client_secret   = var.client_secret != "" ? var.client_secret : var.TF_VAR_client_secret
+  client_id       = var.client_id
+  client_secret   = var.client_secret
   tenant_id       = var.tenant_id
 }
 
