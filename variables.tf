13e074a5c0c96e246a844702fafa3eef159013eb
Jeff T14s <jehonor@outlook.com>
2023-12-21 09:23:22 +0100

    testing

--- variables.tf
+++ variables.tf
@@ -1,13 +1,15 @@
 variable "subscription_id" {
-  default = var.TF_VAR_subscription_id
-}
-variable "client_id" {
-  default = var.TF_VAR_client_id
-}
-variable "client_secret" {
-  default = var.TF_VAR_client_secret
-}
-variable "tenant_id" {
-  default = var.TF_VAR_tenant_id
+  default = var("TF_VAR_subscription_id")
 }
 
+variable "client_id" {
+  default = var("TF_VAR_client_id")
+}
+
+variable "client_secret" {
+  default = var("TF_VAR_client_secret")
+}
+
+variable "tenant_id" {
+  default = var("TF_VAR_tenant_id")
+}
