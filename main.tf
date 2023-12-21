ea7491b62af9a7f4a9123bd3192d607c34883660
Jeff T14s <jehonor@outlook.com>
2023-12-21 07:41:55 +0100

    added login to main.tf to test

--- main.tf
+++ main.tf
@@ -16,6 +16,11 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  client_id       = "e6dce29d-7530-4781-b686-de40b09d5502"
+  client_secret   = "bDL8Q~b7U~InKQ~la5SRB_zcFEbKCruiTUnqocBd"
+  tenant_id       = "9292348b-7b8b-44e5-a9c9-5c933c9e33da"
+  subscription_id = "e77db16d-5ea4-4932-b1f7-066315e7b399"
 }
 
 resource "azurerm_resource_group" "state-demo-secure" {
