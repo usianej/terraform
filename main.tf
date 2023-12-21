9860b544bbead9ac184930639d0aa165d6e34436
Jeff T14s <jehonor@outlook.com>
2023-12-21 07:59:32 +0100

    Reverting back to CLI login

--- main.tf
+++ main.tf
@@ -19,8 +19,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "e77db16d-5ea4-4932-b1f7-066315e7b399"
-  tenant_id       = "9292348b-7b8b-44e5-a9c9-5c933c9e33da"
 }
 
 resource "azurerm_resource_group" "state-demo-secure" {
